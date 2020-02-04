23144fc56db47ce51f032dfba30514cf8e1772ee
Steve Fan <s.fan@uhnresearch.ca>
2020-02-04 16:45:54 -0500

    add summarize_mi_glm()

--- R/fan_util_fun.R
+++ R/fan_util_fun.R
@@ -572,10 +572,90 @@ summarize_coxph<- function(mdl, exponentiate= TRUE, maxlabel= 100, alpha= 0.05) 
   out
 }
 
+summarize_mi_glm<- function(mira_obj, exponentiate= FALSE, alpha= .05) {
+  # mira_obj<- adj_t
+  glm_out<- mira_obj %>%
+    pool() %>%
+    summary(conf.int = TRUE, conf.level = 1-alpha) %>%
+    as.data.frame() %>%
+    rownames_to_column("var") %>%
+    rename(est      = estimate,
+           pval     = p.value,
+           conf_low = `2.5 %`,
+           conf_high= `97.5 %`) %>%
+    mutate(est      = if (exponentiate) exp(est) else est,
+           conf_low = if (exponentiate) exp(conf_low) else conf_low,
+           conf_high= if (exponentiate) exp(conf.high) else conf_high,
+           stat= paste0( formatC(est,       digits = 3, format= "f", flag= "#"), " [",
+                         formatC(conf_low,  digits = 3, format= "f", flag= "#"), ", ",
+                         formatC(conf_high, digits = 3, format= "f", flag= "#"), "]"),
+           pval= format_pvalue(pval)) %>%
+    dplyr::select(var, stat, pval, est, conf_low, conf_high)
 
-summarize_mi_coxph<- function(cox_mira, exponentiate= TRUE) {
+  # to calulate the type 3 error
+  # Li, Meng, Raghunathan and Rubin. Significance levels from repated p-values with multiply-imputed data. Statistica Sinica (1991)
+  x<- model.matrix(mira_obj$analyses[[1]])
+  varseq<- attr(x, "assign")
+  df<- sapply(split(varseq, varseq), length)
+  m <- length(mira_obj$analyses)
 
-  cox_out<- summary(pool(cox_mira)) %>%
+  # coef estimate and its vcov for each MI model
+  betas<- mitools::MIextract(mira_obj$analyses, fun= coef)
+  vars <- mitools::MIextract(mira_obj$analyses, fun= vcov)
+
+  # average betas and vcov cross MI mdls
+  mean_betas<- purrr::reduce(betas, .f= `+`)/m
+  with_var<- purrr::reduce(vars, .f= `+`)/m # with MI
+  # between-MI vcov
+  btwn_var<- lapply(betas, function(cc) (cc - mean_betas) %*% t(cc - mean_betas)) %>%
+    purrr::reduce(.f= `+`)/(m-1)
+
+  out<- lapply(unique(varseq),
+               function(i){
+                 df<- sum(varseq==i)
+                 # set out the contrast matrix
+                 L<- matrix(0, nrow= df, ncol= ncol(x))
+                 L[, varseq==i]<- diag(df)
+
+                 cc<- L %*% mean_betas
+                 vv<- L %*% with_var %*% t(L) # with-mi vcov for beta
+                 v2<- L %*% btwn_var %*% t(L) # btwn-mi vcov for beta
+
+                 # calcualte Wald's test statistics and p-value
+                 rm<- (1 + 1/m) * sum(diag(v2 %*% solve(vv))) # eqn (1.18) without dividing by k
+                 wald_stat<- as.numeric( t(cc) %*% solve(vv) %*% cc )/(df + rm) # eqn (1.17)
+                 # expr (1.19)
+                 nu<- df * (m-1)
+                 df_denominator<- if (nu> 4) {
+                   4 + (nu-4)*(1 + (1-2/nu)/(rm/df))^2
+                 } else {
+                   0.5*(m-1)*(df+1)*(1+1/(rm/df))^2
+                 }
+
+                 pval<- pf(wald_stat, df1= df, df2= df_denominator, lower.tail= FALSE)
+
+                 data.frame(df= round(df, 0),
+                            stat= wald_stat,
+                            chisq_p= pval)
+               })
+  names(out)<- c('(Intercept)',
+                 attr(mira_obj$analyses[[1]]$terms, "term.labels")[unique(varseq)])
+
+  type3_out<- out %>%
+    bind_rows(.id= "var") %>%
+    mutate(pval= format_pvalue(chisq_p)) %>%
+    filter(df>1) %>%
+    dplyr::select(var, pval)
+
+  bind_rows(glm_out, type3_out) %>%
+    arrange(var)
+}
+
+summarize_mi_coxph<- function(cox_mira, exponentiate= TRUE, alpha= .05) {
+
+  cox_out<- cox_mira %>%
+    pool() %>%
+    summary(conf.int = TRUE, conf.level = 1-alpha) %>%
     as.data.frame() %>%
     rownames_to_column("var")
   cox_out<- cox_out[c("var", "est", 'lo 95', 'hi 95', 'Pr(>|t|)')]
